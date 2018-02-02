10f092d59083711c47c5a178a201e30b5866429c
Nick-Desktop <nicholas.zimmermann@gmail.com>
2018-02-01 17:26:11 -0800

    First commit.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position; //gets the distance from the camera to the player object
+    }
+
+    // Update is called once per frame
+    void LateUpdate() //runs every frame, but it is guaranteed to run after all objects have been processed.
+    {
+        transform.position = player.transform.position + offset; //as we move the player, the camera is moved into a new position alligned with player object.
+        //transform.rotation = player.transform.rotation.x;
+    }
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int count;
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    public float jumpHeight;
+
+    private void increment()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 11)
+        {
+            winText.text = "YOU WIN!!!";
+        }
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        increment();
+        winText.text = "";
+    }
+
+    private void Update() //Called before rendering a frame
+    {
+       
+    }
+    private void FixedUpdate() //Called before performing any physics calculations
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical); //used for addforce.
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            Debug.Log(count);
+            increment();
+        }
+    }
+
+   // Destroy(other.gameObject);
+}
